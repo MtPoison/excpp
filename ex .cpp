3d969ac09ebe0d2513c35c919585019ab10faa3f
MtPoison <quentinleforestier0@gmail.com>
2023-10-10 10:45:20 +0200

    first commit

---
+++ ex .cpp
@@ -0,0 +1,52 @@
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+class Airectangle
+{
+public:
+	int width;
+	int heigth;
+
+	int caculAir() {
+		return width * heigth;
+	}
+};
+
+
+int main()
+{
+	/*std::srand(std::time(nullptr));
+	int rand_number = std::rand();
+	int foo = 0;
+	int i = 20;
+
+	while(i >= 0){
+
+		std::cout << "Choisie un chiffre" << std::endl;
+		std::cin >> foo;
+		if (rand_number > foo) {
+			std::cout << "le chiffre rand est plus grand" << std::endl;
+		}
+		if (rand_number < foo) {
+			std::cout << "le chiffre rand est plus petit" << std::endl;
+		}
+		if (rand_number == foo) {
+			std::cout << "gg c'est gagner" << std::endl;
+		}
+		if(i == 0) {
+			std::cout << "c'est perdu, rand = " << rand_number << std::endl;
+		}
+
+		i--;
+	}*/
+
+	Airectangle rect;
+	rect.width = 50;
+	rect.heigth = 60;
+
+	std::cout << "Rectangle area: " << rect.caculAir() << std::endl;
+
+	return 0;
+	
+}
---
+++ ex2.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//namespace 
+//class Airectangle
+//{
+//public:
+//	int x;
+//	int y;
+//
+//	int caculAir() {
+//		return x * y;
+//	}
+//};
+
+//int main(){
+//
+//	Airectangle rect;
+//	rect.x = 9;
+//	rect.y = 15;
+//
+//	std::cout << "Rectangle area: " << rect.caculAir() << std::endl;
+//}
